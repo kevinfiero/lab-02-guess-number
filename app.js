bd6afae5be58ee92cdddfe8724f22d95209bd465
kevinfiero <kjf46@cornell.edu>
2020-09-29 17:26:15 -0700

    basic functionality working

---
+++ app.js
@@ -0,0 +1,105 @@
+// import functions and grab DOM elements
+const startButton = document.getElementById('start-button');
+const inputBox = document.getElementById('input-box');
+const resetButton = document.getElementById('reset-button');
+const guessButton = document.getElementById('guess-button');
+const guessFeedback = document.getElementById('guess-feedback');
+const guessCounterDiv = document.getElementById('guess-counter');
+const quad3Div = document.getElementById('quad3');
+const quad4Div = document.getElementById('quad4');
+const topText = document.getElementById('text-top');
+const bottomText = document.getElementById('text-bottom');
+
+let svgImage = document.getElementById('svg');
+
+// initialize state
+
+// set event listeners to update state and DOM
+
+let targetGuess;
+let guessCounter = 0;
+
+guessButton.addEventListener('click', () => {
+
+    let userGuess = Number(inputBox.value);
+    guessCounter++;
+    guessCounterDiv.textContent = 4 - guessCounter; 
+
+    console.log(targetGuess);
+
+    if (userGuess === targetGuess) {
+        success();
+    } else if (userGuess > targetGuess){
+        svgImage.src = 'assets/arrow.svg';
+        svgImage.style.transform = 'rotate(180deg)';
+        guessFeedback.textContent = 'You need to guess lower!';
+    } else if (userGuess < targetGuess){
+        svgImage.src = 'assets/arrow.svg';
+        svgImage.style.transform = 'rotate(0deg)';
+        guessFeedback.textContent = 'You need to guess higher!';
+    }
+
+    if (guessCounter === 4){
+        if (!(userGuess === targetGuess)) {
+            fail();
+        } else {
+            success();
+        }
+
+
+        return;
+    }
+    
+
+});
+
+startButton.addEventListener('click', () => {
+    targetGuess = Math.floor(Math.random() * 20) + 1;
+    inputBox.style.visibility = 'visible';
+    guessButton.style.visibility = 'visible';
+    guessFeedback.style.visibility = 'visible';
+    startButton.style.visibility = 'hidden';
+    quad3Div.style.visibility = 'visible';
+    quad4Div.style.visibility = 'visible';
+    console.log(guessFeedback.textValue);
+    guessFeedback.textContent = 'Guess a number!';
+
+});
+
+resetButton.addEventListener('click', () => {
+    svgImage.src = 'assets/question.svg';
+    svgImage.style.transform = 'rotate(0deg)';
+    guessCounter = 0;
+    targetGuess = 0;
+    guessCounterDiv.textContent = 4;
+    inputBox.value = '';
+    inputBox.style.visibility = 'hidden';
+    guessButton.style.visibility = 'hidden';
+    guessFeedback.style.visibility = 'hidden';
+    startButton.style.visibility = 'visible';
+    resetButton.style.visibility = 'hidden';
+    guessFeedback.textContent = 'Guess a number!';
+});
+
+function success(){
+    svgImage.src = 'assets/star.svg';
+    svgImage.style.transform = 'rotate(0deg)';
+    guessFeedback.textContent = `Congrats! You guessed the correct value of ${targetGuess}!`;
+    guessButton.style.visibility = 'hidden';
+    inputBox.style.visibility = 'hidden';
+    resetButton.style.visibility = 'visible';
+    topText.textContent = 'You guessed correctly with';
+    guessCounterDiv.textContent = guessCounter;
+    bottomText.textContent = 'attempts!';
+
+}
+
+function fail(){
+    svgImage.src = 'assets/wrong.svg';
+    svgImage.style.transform = 'rotate(0deg)';
+    guessFeedback.textContent = `Sorry, you did not guess the correct value of ${targetGuess}.`;
+    guessButton.style.visibility = 'hidden';
+    inputBox.style.visibility = 'hidden';
+    resetButton.style.visibility = 'visible';
+
+}
